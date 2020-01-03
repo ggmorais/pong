5571447d0d2f85dd5e1ac25f50869b8489bf8e8e
Gustavo Morais <gusxmorais@gmail.com>
2020-01-03 13:40:24 -0300

    Adding some test files

---
+++ src/static/precache-manifest.a94bd65c94b54fbbab208ffa9c0b528f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdf891e264c7882833bd2f8c06d22b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161dcce7df6e29721314",
+    "url": "/static/css/main.c7e66234.chunk.css"
+  },
+  {
+    "revision": "bafcd1827b60216e04f7",
+    "url": "/static/js/2.736f78b5.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/static/js/2.736f78b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "161dcce7df6e29721314",
+    "url": "/static/js/main.1924e744.chunk.js"
+  },
+  {
+    "revision": "9a3ee789b4fff7dbe863",
+    "url": "/static/js/runtime-main.ea70b597.js"
+  }
+]);
